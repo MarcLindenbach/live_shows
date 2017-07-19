7f05f0e915d0546435538cc9a276abba9f2155b7
Marc Lindenbach <marc.lindenbach@gmail.com>
2017-07-19 14:58:12 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => (
+  <h1>Live Shows</h1>
+);
+
+
+console.log([1,2,3].map(n => n + 1));
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const VENDOR_LIBS = [
+  'react',
+  'react-dom'
+];
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+    init: VENDOR_LIBS
+  },
+  output: {
+    path: path.resolve('dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.css$/, use:
+        ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('[name].css'),
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    }),
+  ]
+};
