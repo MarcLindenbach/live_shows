dee3a2e0b051f2f934e76a26007aa4ce7599683c
Marc Lindenbach <marc.lindenbach@gmail.com>
2017-07-20 11:57:22 -0600

    styling/comments

--- src/components/social_media_button.js
+++ src/components/social_media_button.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/*
+ * Social Media Button
+ * Displays a circle social media icon styled for the given type
+ */
 const SocialMediaButton = ({ type, onClick }) => (
   <button
     className={`social-media-button social-media-button--${type}`}
