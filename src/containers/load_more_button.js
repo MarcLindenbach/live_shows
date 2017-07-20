a0c1291b353a4cabcc705cf46f04fbb1f2198a7e
Marc Lindenbach <marc.lindenbach@gmail.com>
2017-07-20 10:43:35 -0600

    load more button shows fetching state

--- src/containers/load_more_button.js
+++ src/containers/load_more_button.js
@@ -4,18 +4,23 @@ import { connect } from 'react-redux';
 import { fetchShows } from '../actions';
 
 const LoadMoreButton = props => (
-  <button onClick={() => props.fetchShows(props.nextPage)}>
-    Load More
+  <button
+    onClick={() => props.fetchShows(props.nextPage)}
+    disabled={props.fetching}
+  >
+    {props.fetching ? 'Loading Shows...' : 'Load More'}
   </button>
 );
 
 LoadMoreButton.propTypes = {
   fetchShows: PropTypes.func.isRequired,
   nextPage: PropTypes.number.isRequired,
+  fetching: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = ({ shows }) => ({
   nextPage: shows.nextPage,
+  fetching: shows.fetching,
 });
 
 const mapDispatchToProps = {
