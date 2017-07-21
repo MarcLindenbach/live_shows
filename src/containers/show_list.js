58132f28ac97244e2d4b3e09b3fdc7f1510fe395
Marc Lindenbach <marc.lindenbach@gmail.com>
2017-07-21 08:52:57 -0600

    removed unused import

--- src/containers/show_list.js
+++ src/containers/show_list.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import { connect } from 'react-redux';
